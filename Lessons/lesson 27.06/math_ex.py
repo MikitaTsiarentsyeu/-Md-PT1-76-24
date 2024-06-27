8d9f8624e1b7e3a71ea054c137a990fa4ff1f468
MikitaTsiarentsyeu <terentievnikita@gmail.com>
2024-06-27 21:21:24 +0300

    add lesson 27/06

---
+++ Lessons/lesson 27.06/math_ex.py
@@ -0,0 +1,41 @@
+import decimal
+import random
+
+x = -33
+print(x, type(x))
+
+print(2+2, 2-4, 2*3, 2**3)
+
+100
+
+x = 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+print(x, type(x))
+
+print(4/2, 4//2)
+
+x = 4/2
+print(x, type(x))
+
+print(0.1+0.1+0.1)
+
+x = 4
+x = float(x)
+print(x, type(x))
+
+x = 4.898998989898989898
+x = int(x)
+print(x, type(x))
+
+print(round(3.5))
+print(round(4.5))
+print(round(5.5))
+print(round(6.5))
+
+
+x = decimal.Decimal(0.1)
+print(x+x+x)
+
+x = decimal.Decimal('0.1')
+print(x+x+x)
+
+# random.randint
---
+++ Lessons/lesson 27.06/pif_enh.py
@@ -0,0 +1,6 @@
+a = float(input("Please enter the a value:"))
+c = float(input("Please enter the c value:"))
+
+b = (c*c - a*a)**0.5
+
+print("The result is", b)
