fe4858680d0cabb9eaa8781a450277f68dba8a16
Arty <arty_h41@mail.ru>
2024-06-29 15:06:52 +0300

    Solved my homework, correction of shortcomings

--- Lavarenka/Task1/task1-4.py
+++ Lavarenka/Task1/task1-4.py
@@ -3,14 +3,9 @@
 store the ratio inside the program itself.
 """
 
-# 4. Напишите программу, конвертирующую некоторую сумму денег из долларов США в белорусские рубли,
-# запросите у пользователя сумму,
-# сохраните соотношение внутри самой программы.
-
+# for more accurate calculations you can use the module Decimal
 dollar = 3.27  # 1$ == 3,27 byn
 
 sum_byn = int(input('Enter dollar amount: ')) * dollar
 
-print(sum_byn)
-
-
+print(f"Your summ: {sum_byn} byn")
