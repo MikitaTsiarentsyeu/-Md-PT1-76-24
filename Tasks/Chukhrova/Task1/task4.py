4c3e04b5df0cd209f1432f6fd1032c2c8b06ac8d
Anna Chukhrova <nyasha1999@mail.ru>
2024-06-30 23:52:19 +0300

    Add solution for tasks 4-5

--- Tasks/Chukhrova/Task1/task4.py
+++ Tasks/Chukhrova/Task1/task4.py
@@ -1,6 +1,16 @@
 '''
-  Write a program that converts some amount of money from USD to BYN, ask a user for the amount, 
+  Write a program that converts some amount of money from USD to BYN, 
+  ask a user for the amount, 
   store the ratio inside the program itself.
 '''
 
-
+try:
+    usd = float(input('Please, input amount of money (USD): '))
+    if usd > 0:
+        RATIO = 3.27
+        byn = round(RATIO * usd, 3)
+        print(f'Amount of money (BYN): {byn}')
+    else:
+        print("Amount of money should be a positive number")
+except ValueError:
+    print('Not a number, please retry')
--- Tasks/Chukhrova/Task1/task5.py
+++ Tasks/Chukhrova/Task1/task5.py
@@ -1,4 +1,25 @@
+import random
+
 '''
-  Write a program that generates a random number in a given range, and shows the answer to a user, 
+  Write a program that generates a random number in a given range, 
+  and shows the answer to a user, 
   ask a user for the left and right border.
 '''
+
+
+left_number_str = input('Please, input left number: ')
+right_number_str = input('Please, input right number: ')
+while True: 
+    if left_number_str.isdigit() and right_number_str.isdigit():
+        left_n = int(left_number_str)
+        right_n = int(right_number_str)
+        rand_n = random.randint(left_n, right_n) if left_n < right_n else random.randint(right_n, left_n)
+        print(rand_n)
+        break
+    elif left_number_str.isdigit():
+        right_number_str = input('Please, input right number: ')
+    elif right_number_str.isdigit():
+        left_number_str = input('Please, input left number: ')
+    else:
+        left_number_str = input('Please, input left number: ')
+        right_number_str = input('Please, input right number: ')
