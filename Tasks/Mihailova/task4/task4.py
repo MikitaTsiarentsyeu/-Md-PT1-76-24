814165ee7dda361ecc9009a263fef1f920f009bd
Alexandra Mikhailava <sasha123456777@gmail.com>
2024-07-27 00:01:19 +0300

    new version of task4

--- Tasks/Mihailova/task4/task4.py
+++ Tasks/Mihailova/task4/task4.py
@@ -4,32 +4,50 @@
 #4. Write the resulting text to a new file and notify the user about it.
 
 import os
-maximum_number = int(input("Maximum number of characters per line(more than 35): "))
 
-#validation
+maximum_number = int(input("Maximum number of characters per line (more than 35): "))
+
+
+while maximum_number <= 35:
+    maximum_number = int(input("Enter a number greater than 35:\n"))
+
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 output_file = os.path.join(script_dir, "text2.txt")
 file_path = r"C:\course\-Md-PT1-76-24\Tasks\Mihailova\task4\text1.txt"
 
+
 with open(file_path, 'r', encoding='utf-8') as file:
     text = file.read()
 
-words = text.split()
-current_line = ""
-formatted_text = ""
+   
+paragraphs = text.split("\n")
+new_text = []
+line = []
+line_length = 0
 
-for word in words:
-    if len(current_line) + len(word) + 1 > maximum_number:
-        formatted_text += current_line + "\n"
-        current_line = word
-    else:
-        if current_line:
-            current_line += " "
-        current_line += word
-
-if current_line:
-    formatted_text += current_line
+for paragraph in paragraphs:
+    words = paragraph.split()
+        
+    for word in words:
+        if line_length + len(word) + (len(line) - 1) < maximum_number:
+                line.append(word)
+                line_length += len(word)
+        else:
+            if line:
+                spaces_to_add = maximum_number - line_length
+                for i in range(spaces_to_add):
+                    line[i % (len(line) - 1)] += ' ' if len(line) > 1 else '' 
+                new_text.append(''.join(line))
+            line = [word]
+            line_length = len(word)
+        
+    if line:
+        new_text.append(' '.join(line))
+        line = []
+        line_length = 0
 
 with open(output_file, 'w', encoding='utf-8') as outfile:
-    outfile.write(formatted_text)
+    outfile.write("\n".join(new_text))
+
 print(f"Отформатированный текст был сохранен в '{output_file}'")
